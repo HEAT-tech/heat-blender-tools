447133b12ee75fb1aee4c564612de099cc851f3a
Alzy <gonzalezalfredo101@gmail.com>
2023-03-01 17:54:33 -0800

    fix incorrect armature rotation when imported

--- operators/api_download_animation.py
+++ operators/api_download_animation.py
@@ -89,6 +89,10 @@ class APIDownloadAnimationOperator(bpy.types.Operator):
                         else:
                             quaternions.append([t, pose[track['name']].to_quaternion().inverted() @ Quaternion((value[3], value[0], value[1], value[2]))])
 
+                        # fix incorrect armature rotation when imported (the animation comes lying down by default)
+                        if track['name'] == 'heat_Root':
+                            quaternions[-1][1] = quaternions[-1][1] + Quaternion((0, -0.707107, 0, 0))
+
                     # fix singularity
                     for j in range(1, len(quaternions)):
                         if quaternions[j][1].dot(quaternions[j-1][1]) < 0:
