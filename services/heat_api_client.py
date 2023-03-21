2f62d339ab702e0e0e89f5fa05ad93ab9ee03ad3
Alzy <gonzalezalfredo101@gmail.com>
2023-03-21 13:11:58 -0700

    CLEANUP

--- services/heat_api_client.py
+++ services/heat_api_client.py
@@ -10,7 +10,6 @@ import certifi
 class HeatAPIClient:
     # base_url = "https://arbztjwhu7.execute-api.us-west-1.amazonaws.com/dev/v1/movements"
     base_url = "https://partner-api.heat.tech/prod/v1/movements"
-    headers = {"X-API-KEY": "HEATDEV"}
     download_dir = os.getenv('TEMP') or '/tmp'
     sslcontext = ssl.create_default_context(cafile=certifi.where())
     timeout = aiohttp.ClientTimeout(total=90)
