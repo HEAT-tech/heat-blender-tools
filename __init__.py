b5075d7d7eca283ce46d9eb5034047e084f9ca16
Alzy <gonzalezalfredo101@gmail.com>
2023-08-08 10:48:10 -0700

    increment version number

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ bl_info = {
     "name": "HeatBlender",
     "author": "Alfredo Gonzalez-Martinez",
     "description": "Heat tools for Blender",
-    "version": (0, 6, 7),
+    "version": (0, 6, 8),
     "blender": (2, 80, 0),
     "location": "View3D",
     "warning": "",
