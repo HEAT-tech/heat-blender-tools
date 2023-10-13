0b36da87fda89d28974670d963cc5622ae7fe91f
Alzy <gonzalezalfredo101@gmail.com>
2023-10-13 14:07:10 -0700

    conditional webui operator

--- operators/open_webui.py
+++ operators/open_webui.py
@@ -1,5 +1,6 @@
 import bpy
 import webbrowser
+from .. import dotenv
 
 class OpenWebUIOperator(bpy.types.Operator):
     """Open Web UI in the default web browser"""
@@ -7,6 +8,16 @@ class OpenWebUIOperator(bpy.types.Operator):
     bl_label = "Open Web UI"
 
     def execute(self, context):
-        url = "http://localhost:5000"
+        local_url = "http://localhost:5000"
+        dev_url = "https://dev-blender.heat.tech"
+        prod_url = "https://app-blender.heat.tech"
+
+        env = dotenv.DotENV()
+        url = prod_url
+        if(env.is_true("HEAT_USE_DEV_API")):
+            url = dev_url
+        if(env.is_true("HEAT_USE_LOCAL_WEBUI")):
+            url = local_url
+
         webbrowser.open(url)
         return {'FINISHED'}
