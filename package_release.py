3f5d862e9d545ae58424b6f8c7e8f1a31ddb3087
Alzy <gonzalezalfredo101@gmail.com>
2023-10-16 09:55:51 -0700

    better dependency packaging

--- package_release.py
+++ package_release.py
@@ -2,7 +2,6 @@ import os
 import zipfile
 from global_vars import VERSION
 
-
 dir_to_zip = os.getcwd()
 
 output_zip = f'build/HeatBlender-v{VERSION}.zip'
@@ -13,13 +12,18 @@ if not os.path.exists(os.path.dirname(output_zip)):
     os.makedirs(os.path.dirname(output_zip))
 
 # List of files and subdirectories to be excluded from the zip file
-exclude_list = ['.git', '.env', '_git_assets', 'local_server.log', 'addon - heatblender_updater', 'build', 'Readme.MD', 'dependencies', '__pycache__', '.gitignore']
+exclude_list = ['.git', '.env', '_git_assets', 'local_server.log', 'addon - heatblender_updater', 'build', 'Readme.MD', '__pycache__', '.gitignore']
 print(f'excluding...{exclude_list}')
 
 with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zip:
     for root, dirs, files in os.walk(dir_to_zip):
         # Remove any excluded files or directories from the list
         dirs[:] = [d for d in dirs if d not in exclude_list]
+
+        # If we're in the 'dependencies' directory, exclude subdirectories with the naming convention "heat-tools-*"
+        if 'dependencies' in root:
+            dirs[:] = [d for d in dirs if not d.startswith('heat-tools-')]
+
         files[:] = [f for f in files if f not in exclude_list]
 
         for file in files:
@@ -27,6 +31,5 @@ with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zip:
             arcname = os.path.relpath(file_path, dir_to_zip)
             print(f'Compressing...{arcname}')
             zip.write(file_path, arcname=os.path.join('HeatBlender', arcname))
-
 
 print('done')
